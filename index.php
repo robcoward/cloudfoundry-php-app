7a48de501a715e1cae2c1398dce729d080bd3e80
Rob Coward <rob@jive-videos.net>
2016-02-19 11:16:29 +0000

    Added more info to the php page

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-phpinfo();
-?>
+<html>
+<head>
+<title>PHP CloudFoundry Demo</title>
+</head>
+<body>
+<table width="100%">
+<tr><td align="center"><h1>Hello from <?php echo $_ENV["CF_INSTANCE_ADDR"]; ?></h1></td></tr>
+<tr><td><?php phpinfo(); ?></td></tr>
+</table>
+</body>
+</html>
