6e05bb63c97da64dc5b03658150f6ab5b7483542
Rob Coward <rob@jive-videos.net>
2016-06-09 22:42:03 +0000

    Added Environment tag to the page.

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
-  $cf_app = json_decode($_ENV["VCAP_APPLICATION"])
+  $cf_app = json_decode($_ENV["VCAP_APPLICATION"]);
+  $server = preg_split("/\./", $_ENV["SERVER_NAME"]);
+
+  switch ($server[0]) {
+    case "staging-php-app":
+      $environment = "Staging Environment";
+      break;
+
+    case "prod-blue-php-app":
+      $environment = "Production Environment (Blue)";
+      break;
+
+    case "prod-green-php-app":
+      $environment = "Production Environment (Green)";
+      break;
+
+    default:
+      $environment = "CloudFoundry PHP Demo";
+  }
 
 ?>
 <html>
@@ -8,7 +26,7 @@
 </head>
 <body>
 <table width="100%">
-<tr><td align="center"><h1><?php echo $cf_app->{'application_name'} ?> served by <?php echo $_ENV["CF_INSTANCE_ADDR"]; ?></h1></td></tr>
+<tr><td align="center"><h1><?php echo $environment; ?></h1><br><h3><?php echo $cf_app->{'application_name'} ?> served by <?php echo $_ENV["CF_INSTANCE_ADDR"]; ?></h3></td></tr>
 <!-- <tr><td><?php phpinfo(); ?></td></tr> -->
 </table>
 </body>
