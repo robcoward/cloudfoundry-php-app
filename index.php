04533a7447339568172ea1b483deea98f18bed91
Rob Coward <rob@jive-videos.net>
2016-06-09 00:20:10 +0000

    Added json parsing to display the CloudFoundry application name.

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+<?php
+  $cf_app = json_decode($_ENV["VCAP_APPLICATION"])
+
+?>
 <html>
 <head>
 <title>PHP CloudFoundry Demo</title>
 </head>
 <body>
 <table width="100%">
-<tr><td align="center"><h1>Served by <?php echo $_ENV["CF_INSTANCE_ADDR"]; ?></h1></td></tr>
+<tr><td align="center"><h1><?php echo $cf_app->{'application_name'} ?> served by <?php echo $_ENV["CF_INSTANCE_ADDR"]; ?></h1></td></tr>
 <tr><td><?php phpinfo(); ?></td></tr>
 </table>
 </body>
