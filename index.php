529b3bb79c60006b4cc1e6ed71239d4706411807
Rob Coward <rob@jive-videos.net>
2016-06-09 21:51:05 +0000

    Commenting out the phpinfo() output.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
 <table width="100%">
 <tr><td align="center"><h1><?php echo $cf_app->{'application_name'} ?> served by <?php echo $_ENV["CF_INSTANCE_ADDR"]; ?></h1></td></tr>
-<tr><td><?php phpinfo(); ?></td></tr>
+<!-- <tr><td><?php phpinfo(); ?></td></tr> -->
 </table>
 </body>
 </html>
