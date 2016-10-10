be9baa0d15acdaab08af965e8e67ee95ab6a91ca
Rob Coward <rob@jive-videos.net>
2016-10-10 23:06:13 +0100

    Updated header

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 </head>
 <body>
 <table width="100%">
-<tr><td align="center"><h1><?php echo $environment; ?></h1><br><h3><?php echo $cf_app->{'application_name'} ?> served by <?php echo $_ENV["CF_INSTANCE_ADDR"]; ?></h3></td></tr>
+<tr><td align="center"><h1><?php echo $environment; ?></h1><br><h3><?php echo $_ENV["OPENSHIFT_BUILD_NAME"] ?> served by <?php echo $_SERVER["SERVER_ADDR"]; ?></h3></td></tr>
 <tr><td><?php phpinfo(); ?></td></tr>
 </table>
 </body>
